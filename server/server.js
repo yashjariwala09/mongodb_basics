f2e1185f846b6eeebe05749e8c51af3e70287c56
lcom133-one <yjariwala25@gmail.com>
2018-12-17 13:24:47 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require(./config/config);
 const _=require('lodash'); 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +9,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 var app = express();
 
 app.use(bodyParser.json());
