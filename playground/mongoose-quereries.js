3f8feb5580fe368eadc68264ef49d61eb1632c22
lcom133-one <yjariwala25@gmail.com>
2018-12-17 10:33:28 +0530

    server.js updated delete route

--- playground/mongoose-quereries.js
+++ playground/mongoose-quereries.js
@@ -1,6 +1,6 @@
 const {mongoose} = require('./../server/db/mongoose');
-const {Todo} = require('./../server/models/todo');
 const {ObjectId} = require('mongodb');
+const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
 
--- server/server.js
+++ server/server.js
@@ -67,6 +67,29 @@ app.get('/todos/:id',(req,res)=>{
 
 
 
+app.delete('/todos/:id',(req,res)=>{
+   
+    var id=req.params.id;
+    console.log(id+"hi");
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo)
+            {
+                return res.status(404).send();
+            }
+            res.status(200).send(todo);
+    },(err)=>{
+     
+        console.log("Unable to insert");
+        
+    }).catch((e)=>{
+    res.status(400).send();
+    });
+});
+
+
 
 
 
